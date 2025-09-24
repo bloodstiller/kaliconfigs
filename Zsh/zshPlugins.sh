85420009ad09a211eb0bc3981faa2deed3f4aea0
bloodstiller <84631290+bloodstiller@users.noreply.github.com>
2025-09-24 12:33:41 +0100

    Update zshPlugins.sh

--- Zsh/zshPlugins.sh
+++ Zsh/zshPlugins.sh
@@ -10,4 +10,4 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 # Install zsh autocomplete
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
