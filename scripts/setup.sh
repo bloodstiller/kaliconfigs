f9cfa6a0451311b2e212f2ed34e2e68503853366
bloodstiller <kali@mymailbullet.com>
2025-12-23 08:59:49 -0500

    alacritty fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -110,6 +110,7 @@ ln -s ~/.dotfiles/Zsh/.zshrc ~/.zshrc
 rm ~/.config/doom/*.el
 ln -s ~/.dotfiles/Doom/*.el ~/.config/doom
 ln -s ~/.dotfiles/Tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/alacritty/alacritty.toml ~/.config
 cd
 ./.config/emacs/bin/doom sync
 
