6a393c4f14cf133ee870038d27a1a968924c8f89
bloodstiller <84631290+bloodstiller@users.noreply.github.com>
2026-06-07 16:09:30 +0100

    added cupp
    added cupp

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -316,7 +316,7 @@ else
         python3-argcomplete atuin flameshot syncthing syncthingtray \
         golang-go ansifilter docker.io docker-buildx docker-compose \
         ntpsec-ntpdate hugo pandoc awscli codelite ruby-dev pyenv jq tmuxinator \
-        alacritty seclists rlwrap azure-cli obsidian jwt penelope
+        alacritty seclists rlwrap azure-cli obsidian jwt penelope cupp
     mark_done "apt"
 fi
 
