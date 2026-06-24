8f2bfee797331851f440bf44f5fd71c4e76d47d6
bloodstiller <84631290+bloodstiller@users.noreply.github.com>
2026-06-24 09:32:53 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -316,7 +316,7 @@ else
         python3-argcomplete atuin flameshot syncthing syncthingtray \
         golang-go ansifilter docker.io docker-buildx docker-compose \
         ntpsec-ntpdate hugo pandoc awscli codelite ruby-dev pyenv jq tmuxinator \
-        alacritty seclists rlwrap azure-cli obsidian jwt penelope cupp rdate fzf 
+        alacritty seclists rlwrap azure-cli obsidian jwt penelope cupp rdate fzf testssl
     mark_done "apt"
 fi
 
